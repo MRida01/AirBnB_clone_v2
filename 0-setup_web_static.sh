c909ba70ac7bc401b559c476220d0e84dae9070c
Mohamed Rida Oukili <lolridalol@gmail.com>
2024-05-06 16:12:17 -0700

    updates

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Bash script to set up web servers for the deployment of web_static
+
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i '/^\tlocation \/hbnb_static {/a\\t\talias /data/web_static/current/;' "$config_file"
+
+sudo service nginx restart
